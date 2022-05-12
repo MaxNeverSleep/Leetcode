70f544dafd1385256f5775920e2aa32e9e49bab5
Yjy <2325265967@qq.com>
2022-05-13 01:14:58 +0800

    Add Question7
    Add Question7

--- app/src/main/java/com/starline/leetcode/Question6.java
+++ app/src/main/java/com/starline/leetcode/Question6.java
@@ -46,7 +46,7 @@ import java.util.HashSet;
 public class Question6 {
 
     public static void main(String... args) {
-        convert(4, "PAYPALISHIRING");
+        System.out.println("final result=" + convert("PAYPALISHIRING", 4));
     }
 
     /**
@@ -56,29 +56,61 @@ public class Question6 {
      * P     I
      * <p>
      * <p>
-     * 0    6     12
-     * 1  5 7   11 13
-     * 2 4  8 10
-     * 3    9
+     * 0   6      12     row=0  6 6     (6=4+2)
+     * 1  5 7   11  13   row=1  4 2 4 2               2=2 3=4 4=6 5=8   2 * (numRows - 1)
+     * 2 4   8 10        row=2  2 4 2                 3=2 4=4 5=6       2 * (numRows - 2)
+     * 3      9          row=3  6                                       2 * (numRows - 3)
+     * <p>                                                              2 * (numRows - 4)
+     * 0   4   8   12    row=0  4 4 4   (4=2+2)
+     * 1 3 5 7 9 11      row=1  2 2 2 2 2
+     * 2   6   10        row=2  4 4
      * <p>
-     * 0   4   8   12
-     * 1 3 5 7 9 11
-     * 2   6   10
-     * <p>
-     * 0     8        16   row=0
-     * 1   7  9      15    row=1
-     * 2  6   10   14      row=2
-     * 3 5     11 13       row=3
-     * 4         12        row=4
+     * 0     8          row=0   8       (8=6+2)      0      1       2
+     * 1   7 9    15    row=1   6 2 6                3     4 5     6
+     * 2  6  10  14     row=2   4 4 4 4              7    8  9    10
+     * 3 5   11 13      row=3   2 6 2                11 12   13 14
+     * 4     12         row=4   8                    15       16
      */
-    public static String convert(int rowNum, String str) {
-        StringBuilder convert = new StringBuilder();
-        System.out.println(4 * (rowNum - 1));
+    public static String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
 
-//        for (int i = 0; i < str.length(); i++) {
-//        }
+        StringBuilder sb = new StringBuilder();
+        int index;
+        int maxGap = 2 * (numRows - 1);
+        int gap;
+        boolean needMinusGap = false; //标记，处理 6 2 6 2 这种情况的行，如果是true 则需要用firstGap-Gap 比如 6 2 6 2
 
-        return "";
+        for (int i = 0; i < numRows; i++) {
+            gap = 2 * (numRows - (i + 1));
+            index = i;
+
+            System.out.println("row= " + i);
+            System.out.println("gap= " + gap);
+
+            while (index < s.length()) {
+                sb.append(s.charAt(index));
+                System.out.println("append index= " + index);
+                if (maxGap != gap) {
+                    if (gap == 0) {
+                        index += maxGap;
+                    } else {
+                        if (needMinusGap) {
+                            index += Math.abs(maxGap - gap);
+                            needMinusGap = false;
+                        } else {
+                            index += gap;
+                            needMinusGap = true;
+                        }
+                    }
+                } else {
+                    index += gap;
+                }
+            }
+            needMinusGap = false;
+        }
+        return sb.toString();
     }
 
 }
---
+++ app/src/main/java/com/starline/leetcode/Question7.java
@@ -0,0 +1,77 @@
+package com.starline.leetcode;
+
+/**
+ * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+ * <p>
+ * 如果反转后整数超过 32 位的有符号整数的范围[−2³¹, 2³¹− 1] ，就返回 0。
+ * <p>
+ * 假设环境不允许存储 64 位整数（有符号或无符号）。
+ * <p>
+ * 示例 1：
+ * 输入：x = 123
+ * 输出：321
+ * <p>
+ * 示例 2：
+ * 输入：x = -123
+ * 输出：-321
+ * <p>
+ * 示例 3：
+ * 输入：x = 120
+ * 输出：21
+ * <p>
+ * 示例 4：
+ * 输入：x = 0
+ * 输出：0
+ */
+public class Question7 {
+
+    public static void main(String... args) {
+        System.out.println(reverse(363847412));
+//        2147483648
+//        214748363
+//        214748363
+    }
+
+    public static int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            result = x % 10 + result * 10;
+            if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+                return 0;
+            }
+            x = x / 10;
+        }
+        return result;
+    }
+
+//    public static int reverse(int x) {
+//        boolean isNegative = false;
+//        if (x < 0) {
+//            isNegative = true;
+//        }
+//        String s = String.valueOf(Math.abs(x));
+//        StringBuilder sb = new StringBuilder();
+//        if (s.length() != 1) {
+//            for (int i = s.length() - 1; i >= 0; i--) {
+//                if (i == s.length() - 1) {
+//                    if (s.charAt(i) == '0') {
+//                        continue;
+//                    }
+//                }
+//                sb.append(s.charAt(i));
+//            }
+//            try {
+//                int result = Integer.parseInt(sb.toString());
+//                if (isNegative) {
+//                    return -result;
+//                } else {
+//                    return result;
+//                }
+//            } catch (NumberFormatException e) {
+//                return 0;
+//            }
+//        }
+//        return x;
+//    }
+
+}
